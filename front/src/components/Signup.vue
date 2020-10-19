e37c101ebedb8f9739e949389a021d7408c235c2
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-10-19 15:38:28 +0900

    #8 create simple signup form

--- front/src/components/Signup.vue
+++ front/src/components/Signup.vue
@@ -1,9 +1,32 @@
 <template>
-    <h1>signup</h1>
+<v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title>
+            <h1 class="display-1">ログイン</h1>
+        </v-card-title>
+        <v-card-text>
+            <v-form>
+                <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
+                <v-text-field prepend-icon="mdi-lock" 
+                    v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" 
+                    v-bind:type="showPassword ? 'text' : 'password'" 
+                    @click:append="showPassword = !showPassword"
+                    label="パスワード" />
+                <v-card-actions>
+                    <v-btn>ログイン</v-btn>
+                </v-card-actions>
+            </v-form>
+        </v-card-text>
+    </v-card>
+</v-app>
 </template>
 
 <script>
 export default {
+    name: 'login',
+    data: () => ({
+        showPassword : false
+    })
     
 }
 </script>
