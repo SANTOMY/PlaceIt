e2fc10527535a7c24fd942f315fc2b4f8ad39e76
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2020-10-20 13:08:21 +0900

    #31 Map 編集中

--- front/src/components/Map.vue
+++ front/src/components/Map.vue
@@ -1,9 +1,23 @@
 <template>
-    <h1>map</h1>
+  <div id="map">
+    <l-map ref="map" style="height:600px;" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+  </div>
 </template>
 
 <script>
+import { LMap, LTileLayer } from "vue2-leaflet";
+
 export default {
-    
-}
-</script>
+  name: "map",
+  components: { LMap, LTileLayer },
+  data() {
+    return {
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      zoom: 8,
+      center: [35.693825, 139.703356],
+    };
+  }
+};
+</script> 
