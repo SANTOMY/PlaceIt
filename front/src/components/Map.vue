3d2a611311378c4bd9c659a264b13908ea7da4f3
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2020-10-26 13:21:56 +0900

    make map template

--- front/src/components/Map.vue
+++ front/src/components/Map.vue
@@ -1,23 +1,62 @@
 <template>
-  <div id="map">
-    <l-map ref="map" style="height:600px;" :zoom="zoom" :center="center">
-      <l-tile-layer :url="url"></l-tile-layer>
-    </l-map>
+  <div id='map'>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer } from "vue2-leaflet";
+import  'leaflet/dist/leaflet.css'
+import  L from 'leaflet'
+
+delete  L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions(
+    {   iconUrl         : require( 'leaflet/dist/images/marker-icon.png' )
+    ,   iconRetinaUrl   : require( 'leaflet/dist/images/marker-icon-2x.png' )
+    ,   shadowUrl       : require( 'leaflet/dist/images/marker-shadow.png' )
+    }
+)
 
 export default {
-  name: "map",
-  components: { LMap, LTileLayer },
-  data() {
-    return {
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-      zoom: 8,
-      center: [35.693825, 139.703356],
-    };
+    data: function(){
+      return {
+      lat:0,//緯度
+      lon:0,//経度
+      map: null,
+      zoom:15,
+      counter:0,
+      };
+    },
+    
+    mounted() {
+      //テストの値
+      this.lon = 135;
+      this.lat = 35;
+
+      //Mapオブジェクトの生成
+      this.map = L.map('map')
+      .setView([this.lat, this.lon],this.zoom)
+      .addLayer(
+        L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+          attribution:
+            'Map data &copy <a href="https://openstreetmap.org">OpenStreetMap</a>'
+        })
+      );
+
+      //位置情報検索
+      this.map.locate({ setView: true,maxZoom: 20})
+    }
+}
+</script>
+
+<style>
+html,
+body,
+#map { 
+  height: 100%; 
+  width: 100%;
   }
-};
-</script> 
+body {
+  margin: 0;
+  height: 100%;
+}
+</style>
