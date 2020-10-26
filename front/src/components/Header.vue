0fc63595c6df744b0e4bd3b44e946cd8728a51ca
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-10-26 13:46:01 +0900

    add header

---
+++ front/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app-bar app>
+        <v-toolbar-title>Application</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-row justify="space-around">
+            <v-icon
+            large
+            color="green darken-2"
+            >
+            mdi-domain
+            </v-icon>
+
+            <v-icon
+            large
+            color="blue darken-2"
+            >
+            mdi-message-text
+            </v-icon>
+        </v-row>
+    </v-app-bar>
+</template>
+
+
+
