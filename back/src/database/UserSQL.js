49d7c03ea03e944859af29a5e77420d5130c4da1
ONourry <onourry3@gmail.com>
2020-10-26 17:33:54 +0900

    #11 Adding promise to db code for async

--- back/src/database/UserSQL.js
+++ back/src/database/UserSQL.js
@@ -1,25 +1,39 @@
 'use strict';
+const con = require('./DBHandler.js');
+const {info, debug, warning, error}  = require('../winston');
+const { connect } = require('../route/userRoute.js');
+const connection = con.connection;
+const fileLabel = "UserSQL"
+const User = require('../objects/user');
 
-class UserSQL {
-    constructor(id, name, mail, password) {
-        this.id = id;
-        this.name = name;
-        this.mail = mail;
-        this.password = password;
-    }
+/**
+ * Add user data to the database
+ */
+async function saveUser(newUser) {
+    const query = {
+        text: 'INSERT INTO users.users(id, username, email, password) VALUES($1, $2, $3, $4)',
+        values: [newUser.userId, newUser.userName, newUser.email, newUser.password]
+    };
+    connection.connect().then(()=>{
+        //Need to add check if email/username already exists in database
+        return connection.query(query).then((newUser)=>{
+            connection.end().then(()=>{
+                debug(fileLabel,"saved user: " + newUser);
+                return {"sucess":true,"data":newUser};
+            });
+            
 
-    /**
-     * Add user data to the database
-     */
-    addToDatabase() {
-        const conf = require('./DBHandler.js')
-        const query = {
-            text: 'INSERT INTO users.users(id, username, email, password) VALUES($1, $2, $3, $4)',
-            values: [this.id, this.name, this.mail, this.password]
-        }
-        const queries = [query]
-        conf.connect(queries)
-    }
+        }).catch((exception)=>{
+            connection.end().then(()=>{
+                error(fileLabel,"Error while saving user: " + exception);
+                return {"success":false,"data":exception};
+            });
+        });
+    }).catch((exception)=>{
+        error(fileLabel,"Error trying to connect to database: " + exception);
+        return {"sucess":false,"data":exception};
+    });
 }
 
-//new User(2, 'jiro', 'bbb@com', 'pass2').addToDatabase()
+const testUser = new User(1000,'testName','some@Email','testPassword');
+saveUser(testUser);
---
+++ back/src/objects/user.js
@@ -0,0 +1,10 @@
+
+module.exports = class User{
+    constructor(id, name, mail, password) {
+        this.id = id;
+        this.name = name;
+        this.mail = mail;
+        this.password = password;
+    }
+}
+
