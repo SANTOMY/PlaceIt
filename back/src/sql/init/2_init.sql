016a6377bfabb33bf9efd2a1a45ca9463af3c54c
Shimizu Kazuki <shimizu@poslnoMacBook-Pro-2.local>
2020-10-26 17:04:39 +0900

    Fix the problem with sql code

--- back/src/sql/init/2_init.sql
+++ back/src/sql/init/2_init.sql
@@ -1,16 +1,17 @@
 create table spots.spots
 (
-  spot_id serial primary key,
-  spot_name varchar(50) unique not null,
-  geom GEOMETRY(),
-  spot_type varchar(50) unique not null,
-  this.user_id bigint;
+  spot_id bigint primary key,
+  spot_name varchar(50),
+  geom GEOMETRY,
+  spot_type varchar(50),
+  picture varchar(50)
+  user_id bigint
 );
 create table spots.review
 (
-  review_id serial primary key,
+  review_id bigint primary key,
   spot_id bigint,
-  comment varchar(255) unique not null,
+  comment varchar(255) not null,
   score bigint,
   user_id bigint
 );
