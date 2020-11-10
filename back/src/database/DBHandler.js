fae9b2d0d190926ba307c4ecd9b804c595d8e2f6
ONourry <onourry3@gmail.com>
2020-11-10 14:42:34 +0900

    #27 removed unused connection code

--- back/src/database/DBHandler.js
+++ back/src/database/DBHandler.js
@@ -5,15 +5,6 @@ const env = dotenv.config();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-// const client = new Client ({
-//     database: env.parsed.DB,
-//     user: env.parsed.USER, //ユーザー名はデフォルト以外を利用している人は適宜変更してください。
-//     password: env.parsed.PASSWORD, //PASSWORDにはPostgreSQLをインストールした際に設定したパスワードを記述。
-//     host: env.parsed.HOST,
-//     port: 5432,
-//     ssl: true
-// });
-
 const pool = new pg.Pool({
     user: env.parsed.USER,
     host: env.parsed.HOST,
@@ -23,4 +14,4 @@ const pool = new pg.Pool({
     ssl: true
   })
 
-module.exports={pool:pool,connection:null}
+module.exports={pool:pool}
