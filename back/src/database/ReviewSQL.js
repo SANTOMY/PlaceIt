bfa2d370185f03337455923ed559ed5b4e05d6dc
Shimizu Kazuki <shimizu@poslnoMacBook-Pro-2.local>
2020-11-17 13:15:33 +0900

    #24 Change connect function client to pool

--- back/src/database/ReviewSQL.js
+++ back/src/database/ReviewSQL.js
@@ -2,6 +2,8 @@
 const con = require('./DBHandler.js');
 const connection = con.connection;
 const {info, debug, warning, error}  = require('../winston');
+const { connect } = require('../route/reviewRoute.js');
+const pool = con.pool;
 const fileLabel = "ReviewSQL"
 const Review = require('../objects/review');
 const util = require('util');
@@ -11,22 +13,18 @@ module.exports.saveReview = async function(newReview){
         text: 'INSERT INTO spots.review(review_id, spot_id, comment, score, user_id) VALUES($1, $2, $3, $4, $5);',
         values: [newReview.reviewId, newReview.spotId, newReview.comment, newReview.score, newReview.userId]
     };
-    return connection.connect().then(()=>{
-        //TODO: NULL CHECK
-        return connection.query(query)
-        .then(()=>{
-                connection.end()
-                debug(fileLabel,"saved review: " + newReview);
-                return {"success":true,"data":newReview};
-        })
-        .catch((exception)=>{
-            connection.end()
-            error(fileLabel,"Error while saving review: " + exception);
-            return {"success":false,"data":exception};
-        });
-    }).catch((exception)=>{
-        error(fileLabel,"Error trying to connect to database: " + exception);
-        return {"success":false,"data":exception};
+    const client = await pool.connect();
+    return client.query(query)
+    .then(() =>{
+        client.release();
+        debug(fileLabel,"saved review: " + newReview);
+        return {"success":true,"data":newReview};
+    })
+    .catch((err)=>{
+        client.release();
+        info(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
+        error(fileLabel,"Error while saving review: " + err);
+        return {"success":false,"data":err};
     });
 }
 
@@ -34,22 +32,18 @@ module.exports.getReviewBySpotId = async function(spotId){
     const query = {
         text: `SELECT * FROM spots.review WHERE spot_id='${spotId}'`
     };
-    return connection.connect().then(()=>{
-        return connection.query(query).then( result => {
-            connection.end();
-            if (result.rowCount == 0)
-                return {"success":false, "data":"Review does not exist"};
-            info(fileLabel,"get review by spot_id: " + util.inspect(spotId,{showHidden: false, depth: null}));
-            return {"success":true, "data":result.rows};
-        }).catch((exception)=>{
-            connection.end();
-            error(fileLabel,"Error while getting review. " + exception);
-            return {"success":false, "data":exception};      
-        });
-    }).catch((exception)=>{
-        connection.end();
-        error(fileLabel,"Error trying to connect to database: " + exception);
-        return {"success":false, "data":exception};
+    const client = await pool.connect();
+    return client.query(query).then( result => {
+        client.release();
+        if (result.rowCount == 0)
+            return {"success":false, "data":"Review does not exist"};
+        info(fileLabel,"get review by spot_id: " + util.inspect(spotId,{showHidden: false, depth: null}));
+        return {"success":true, "data":result.rows};
+    }).catch((err)=>{
+        client.release();
+        info(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
+        error(fileLabel,"Error while getting review. " + err);
+        return {"success":false, "data":err};      
     });
 }
 
