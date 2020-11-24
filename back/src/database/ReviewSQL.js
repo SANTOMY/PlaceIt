f27074b2e83f861f4ea599681aec126a4d8eed60
ONourry <onourry3@gmail.com>
2020-11-24 14:36:11 +0900

    refactored empty value check, added check for email in saveUserSQL

--- back/src/database/ReviewSQL.js
+++ back/src/database/ReviewSQL.js
@@ -6,6 +6,8 @@ const pool = con.pool;
 const fileLabel = "ReviewSQL"
 const Review = require('../objects/review');
 const util = require('util');
+const utility = require('../utility');
+const { isEmpty } = require('../utility');
 
 module.exports.saveReview = async function(newReview){
     const checkQuery = {
@@ -16,19 +18,18 @@ module.exports.saveReview = async function(newReview){
         text: 'INSERT INTO spots.review(review_id, spot_id, comment, score, user_id) VALUES($1, $2, $3, $4, $5);',
         values: [newReview.reviewId, newReview.spotId, newReview.comment, newReview.score, newReview.userId]
     };
+
+    if(utility.isEmpty(newReview.spotName)){
+        error(fileLabel,"ERROR OBJECT:" + util.inspect(newReview.spotId,{showHidden: false, depth: null}));
+        error(fileLabel,"Spot name is Undefined" + newReview.spotName);
+        return {"success":false,"data":"Spot Id is Undefined"};
+    }
     const client = await pool.connect();
     return client.query(checkQuery)
     .then( nullchecker =>{
-        if(typeof newReview.spotName === 'undefined'){
+         if(nullchecker.rowCount == 0){
             client.release();
-            info(fileLabel,"ERROR OBJECT:" + util.inspect(newReview.spotId,{showHidden: false, depth: null}));
-            error(fileLabel,"Spot Id is Undefined" + newReview.spotId);
-            return {"success":false,"data":"Spot Id is Undefined"};
-        }else if(nullchecker.rowCount == 0){
-            client.release();
-            info(fileLabel,"ERROR OBJECT:" + util.inspect(newReview.spotId,{showHidden: false, depth: null}));
-            error(fileLabel,"There is no spot at: " + newReview.spotId);
-            return {"success":false,"data":newReview.spotId};
+            return {"success":false,"data":"This spot does no exist: " + newSpot.spotName};
         }
         return client.query(query)
         .then(() =>{
@@ -38,7 +39,7 @@ module.exports.saveReview = async function(newReview){
         })
         .catch((err)=>{
             client.release();
-            info(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
+            error(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
             error(fileLabel,"Error while saving review: " + err);
             return {"success":false,"data":err};
         });
@@ -54,12 +55,12 @@ module.exports.getReviewBySpotId = async function(spotId){
     return client.query(query).then( result => {
         client.release();
         if (result.rowCount == 0)
-            return {"success":false, "data":"Review does not exist"};
+            return {"success":false, "data":"No reviews exist for this spot"};
         info(fileLabel,"get review by spot_id: " + util.inspect(spotId,{showHidden: false, depth: null}));
         return {"success":true, "data":result.rows};
     }).catch((err)=>{
         client.release();
-        info(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
+        error(fileLabel,"ERROR OBJECT:" + util.inspect(err,{showHidden: false, depth: null}));
         error(fileLabel,"Error while getting review. " + err);
         return {"success":false, "data":err};      
     });
