f7f59a527f671b95a2b7d34812ace12cca3ea9b6
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2020-12-01 13:57:01 +0900

    #51 Change user/login -> login

--- back/src/route/userRoute.js
+++ back/src/route/userRoute.js
@@ -2,52 +2,9 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('./userController');
 const userController = new UserController();
-const passport = require('./auth');
-const session = require('express-session');
-const flash = require('connect-flash');
-
-router.use(flash());
-router.use(session({
-    secret: 'YOUR-SECRET-STRING',
-    resave: true,
-    saveUninitialized: true
-}));
-router.use(passport.initialize());
-router.use(passport.session());
-
-const authMiddleware = (req, res, next) => {
-    if(req.isAuthenticated()) {
-        next();
-    } else {
-        res.redirect(301, '/user/notLogin');
-    }
-};
 
 router.post("/register",userController.register);
 router.get("/getUserByEmail",userController.getUserByEmail);
 router.put("/editUser",userController.editUser);
-router.post("/login",
-    passport.authenticate('local', {
-        successRedirect: '/user/userpage',
-        failureRedirect: '/user/loginFail',
-        failureFlash: true,
-    })
-);
-router.post('/logout', (req, res) => {
-    req.session.passport.user = undefined;
-    res.redirect('/');
-});
-// login succeccful test page
-router.get('/userpage', authMiddleware, (req, res) => {
-    res.send('login successful');
-});
-// login fail in test page
-router.get('/loginFail', (req, res) => {
-    res.send('login fail');
-});
-// not logged in test page
-router.get('/notLogin', (req, res) => {
-    res.send('not logged in');
-});
 
 module.exports = router;
--- back/src/server.js
+++ back/src/server.js
@@ -9,14 +9,50 @@ const userRoute = require('./route/userRoute');
 const spotRoute = require('./route/spotRoute');
 const reviewRoute = require('./route/reviewRoute');
 
+const passport = require('./route/auth');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+app.use(flash());
+app.use(session({
+    secret: 'YOUR-SECRET-STRING',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const authMiddleware = (req, res, next) => {
+    if(req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect(301, '/');
+    }
+};
+
 app.use(cors())
 
 app.use(bodyParser.json())
-app.use('/user',userRoute)
+app.use('/user',authMiddleware, userRoute)
 app.use('/spot',spotRoute)
 app.use('/review',reviewRoute)
 
 
+
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) {return res.status(400).json({"success": false, "error": err})};
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.status(200).json({"success": true});
+        });
+       })(req, res, next);
+});
+
+app.post('/logout', (req, res) => {
+    req.logout();
+});
 
 app.get('/', (req, res) => {
   console.log(req);
