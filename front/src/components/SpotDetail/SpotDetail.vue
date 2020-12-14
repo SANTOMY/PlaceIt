7ade2b02b1b1ea6a989d491c89077ff446f44a5a
Asada0815 <asada@posl.ait.kyushu-u.ac.jp>
2020-12-14 17:12:11 +0900

    #94 change to dialog

--- front/src/components/SpotDetail/SpotDetail.vue
+++ front/src/components/SpotDetail/SpotDetail.vue
@@ -1,86 +1,102 @@
 <template>
-    <v-container>
+    <v-dialog
+      v-model="showDialog"
+      width="1200"
+    >
+        <!-- ダイアログを開くボタン -->
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn 
+                class="px-10 py-6"
+                block
+                v-bind="attrs"
+                v-on="on"
+            >
+                <h2>OPEN</h2>
+            </v-btn>
+        </template>
         <v-card>
-            <!-- 写真 -->
-            <v-carousel
-                cycle
-                hide-delimiter-background
-                show-arrows-on-hover
-            >
-                <v-carousel-item
-                    v-for="photo in spot_data.photos"
-                    :key="photo"
-                    :src="photo"
-                />
-            </v-carousel>
-            <v-row
-                justify="center"
-                class="mx-10
-                mt-3 mb-5"
-            >
-                <!-- スポット名 -->
-                <h1 class="mr-10"> {{ spot_data.name }} </h1>
+            <v-container>
 
-                <!-- スポットタイプ -->
-                <spot-type-icon v-for="type in spot_data.types" :key="type"
-                    :type="type"
-                    class="mr-5"
-                    large
-                    color="gray"
-                />
+                <!-- 写真 -->
+                <v-carousel
+                    cycle
+                    hide-delimiter-background
+                    show-arrows-on-hover
+                >
+                    <v-carousel-item
+                        v-for="photo in spot_data.photos"
+                        :key="photo"
+                        :src="photo"
+                    />
+                </v-carousel>
+                <v-row
+                    justify="center"
+                    class="mx-10
+                    mt-3 mb-5"
+                >
+                    <!-- スポット名 -->
+                    <h1 class="mr-10"> {{ spot_data.name }} </h1>
 
-            </v-row>
+                    <!-- スポットタイプ -->
+                    <spot-type-icon v-for="type in spot_data.types" :key="type"
+                        :type="type"
+                        class="mr-5"
+                        large
+                        color="gray"
+                    />
 
-            <v-row>
-                <!-- スポットの評価 -->
-                <v-col>
-                    <v-row justify="center">
-                        <star-rating
-                            v-model="spot_data.rating"
-                            read-only
-                            :increment=0.5
-                        >
-                        </star-rating>
-                    </v-row>
-                </v-col>
+                </v-row>
 
-                <!-- レビュー 一覧 -->
-                <v-col>
-                    <v-row justify="center">
-                        <v-col cols="11">
-                            <spot-review-list :reviews="sliced_reviews" />
-                        </v-col>
-                    </v-row>
+                <v-row>
+                    <!-- スポットの評価 -->
+                    <v-col>
+                        <v-row justify="center">
+                            <star-rating
+                                v-model="spot_data.rating"
+                                read-only
+                                :increment=0.5
+                            >
+                            </star-rating>
+                        </v-row>
+                    </v-col>
 
-                    <v-row class="mt-1">
-                        <v-icon
-                            class="ml-8"
-                            large
-                            @click="change_page(-1)"
-                        >
-                            mdi-chevron-left
-                        </v-icon>
-                        <v-icon
-                            class="ml-6"
-                            large
-                            @click="change_page(1)"
-                        >
-                            mdi-chevron-right
-                        </v-icon>
-                    </v-row>
+                    <!-- レビュー 一覧 -->
+                    <v-col>
+                        <v-row justify="center">
+                            <v-col cols="11">
+                                <spot-review-list :reviews="sliced_reviews" />
+                            </v-col>
+                        </v-row>
 
-                    <v-row justify="center">
-                        <v-col cols="5">
+                        <v-row class="mt-1">
+                            <v-icon
+                                class="ml-8"
+                                large
+                                @click="change_page(-1)"
+                            >
+                                mdi-chevron-left
+                            </v-icon>
+                            <v-icon
+                                class="ml-6"
+                                large
+                                @click="change_page(1)"
+                            >
+                                mdi-chevron-right
+                            </v-icon>
+                        </v-row>
 
-                            <spot-review-register />
-                        </v-col>
-                    </v-row>
+                        <v-row justify="center">
+                            <v-col cols="5">
 
-                </v-col>
-            </v-row>
+                                <spot-review-register />
+                            </v-col>
+                        </v-row>
+
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-card>
-    </v-container>
-
+    </v-dialog>
 </template>
 
 <script>
