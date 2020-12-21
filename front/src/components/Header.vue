a57d65fad020d27c980106e27b3fecdc3c3b97c8
ONourry <onourry3@gmail.com>
2020-12-21 13:41:15 +0900

    #92 moved logout button to user menu

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -23,23 +23,27 @@
             </v-icon>
             Map
         </v-btn>
-
-        <v-btn
-            to="/user"
-            class="px-10 mx-2"
-            text
-            v-if="isLoggedIn"
+        
+        <v-menu 
+          offset-y :close-on-click=false
+          close-on-content-click=false
+          v-if="isLoggedIn"
         >
-            <v-icon
-                left
-                large
-                color="gray"
-                class="px-5"
-            >
-                mdi-account-circle
-            </v-icon>
-            User
-        </v-btn>
+            <template v-slot:activator="{ on }">
+                <v-btn class="px-10 mx-2" v-if="isLoggedIn" v-on="on">
+                    <v-icon left large color="gray" class="px-5"> mdi-account-circle</v-icon>
+                    User
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item>
+                    <v-btn to="/user" class="px-8 mb-2">Profile</v-btn>
+                </v-list-item>
+                <v-list-item>
+                    <v-btn @click="logout" class="px-8">Logout</v-btn>
+                </v-list-item>
+            </v-list>
+        </v-menu>
 
         <v-btn
             to="/login"
@@ -57,36 +61,31 @@
             </v-icon>
             Login
         </v-btn>
-
-
-        <v-btn
-            @click="logout"
-            class="px-10 mx-2"
-            text
-            v-if="isLoggedIn"
-        >
-            <v-icon
-                left
-                large
-                color="gray"
-                class="px-5"
-            >
-                mdi-login
-            </v-icon>
-            Logout
-        </v-btn>
     </v-app-bar>
 </template>
 
 <script>
 export default {
     
+    data (){
+        return {items: [
+                {option: 'Profile', src: '/user'},
+                {option: 'Logout', src: '/map'}
+            ]
+        }
+    },
     computed: {
         isLoggedIn() {
             return this.$store.state.userData != null
         }
     },
     methods: {
+        menuOptions(menuOption){
+            if (menuOption.option == 'Logout'){
+                this.$store.commit("logout")
+            }
+            this.$router.push({path: menuOption.src})
+        },
         logout() {
             this.$store.commit("logout")
             if (this.$route.path != '/map'){
