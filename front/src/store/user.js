1a06680363b098a091e83625afa46b4b8390c796
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2021-01-13 13:39:50 +0900

    fix front routing to add university

--- front/src/store/user.js
+++ front/src/store/user.js
@@ -1,16 +1,18 @@
 module.exports = class User{
-    constructor(userId, userName, email, password) {
+    constructor(userId, userName, email, password, university) {
         this.userId = userId;
         this.userName = userName;
         this.email = email;
         this.password = password;
+        this.university = university;
     }
 
     getJSON(){
         let user = {
             "userId": this.userId,
             "userName": this.userName,
-            "email": this.email
+            "email": this.email,
+            "university": this.university
         };
       return user;
     }
