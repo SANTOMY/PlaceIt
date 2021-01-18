6844f8262b6d8161495e04e6629f06d9697399f7
tsukasa2 <t.nakamura@posl.ait.kyushu-u.ac.jp>
2021-01-18 13:46:15 +0900

    add score1-5 to Review class

--- back/src/objects/review.js
+++ back/src/objects/review.js
@@ -1,9 +1,14 @@
 module.exports = class Review {
-    constructor(reviewId, spotId, comment, score, userId){
+    constructor(reviewId, spotId, comment, score, userId, score1, score2, score3, score4, score5){
         this.reviewId = reviewId;
         this.spotId = spotId;
         this.comment = comment;
         this.score = score;
         this.userId = userId;
+        this.score = score1;
+        this.score = score2;
+        this.score = score3;
+        this.score = score4;
+        this.score = score5;
     }
 }
