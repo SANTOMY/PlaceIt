ccf2b830f810ecbc70b23b78d45ed7b2a6206850
ShimizuKazuki <shimizu@posl.ait.kyushu-u.ac.jp>
2021-04-15 16:45:24 +0900

    Reset database setting

--- back/src/database/DBHandler.js
+++ back/src/database/DBHandler.js
@@ -6,9 +6,9 @@ const env = dotenv.config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const pool = new pg.Pool({
-    user: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: 5432,
     ssl: true
