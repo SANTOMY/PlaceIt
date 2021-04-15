68774949c7e3b9d345a2afd7d2a88b312f653d49
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2021-04-15 15:48:55 +0900

    Change origin

--- front/src/routes/userRequest.js
+++ front/src/routes/userRequest.js
@@ -9,7 +9,7 @@ async function register(userName, email, password, university){
             headers:{
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Origin': 'http://localhost:8080'
+                'Origin': 'http://localhost'
             },
             body: JSON.stringify({userName: userName, email: email, password: password, university: university})
         });
@@ -49,7 +49,7 @@ async function editUser(currentEmail,newEmail,newPassword,newUserName){
             headers:{
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Origin': 'http://localhost:8080'
+                'Origin': 'http://localhost'
             },
             body: JSON.stringify({currentEmail: currentEmail, newEmail: newEmail, newPassword: newPassword, newUserName: newUserName})
         });
@@ -68,7 +68,7 @@ async function login(email,password){
             headers:{
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Origin': 'http://localhost:8080'
+                'Origin': 'http://localhost'
             },
             body: JSON.stringify({email: email, password: password})
         });
