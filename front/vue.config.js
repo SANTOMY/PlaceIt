c530418b6607abbb1a22bb699348d1f91978f2c3
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2021-04-21 16:37:34 +0900

    Fix Dockerfile

--- front/vue.config.js
+++ front/vue.config.js
@@ -3,21 +3,15 @@ const vuetifyOptional = process.env.NODE_ENV === "production"
   : [];
 
 module.exports = {
-  // configureWebpack: {
-  //   devServer: {
-  //       //public: 'https://place-it-front.herokuapp.com',
-  //       host: '0.0.0.0',
-  //       port: process.env.PORT　|| "8080",
-  //   },
-  //   plugins: [
-  //     ...vuetifyOptional,
-  //   ],
-  // },
-  // transpileDependencies: vuetifyOptional.length > 0 ? ["vuetify"] : [],
-  devServer: {
-    // public: 'https://place-it-front.herokuapp.com',
-    host: '0.0.0.0',
-    port: process.env.PORT　|| "8080",
-    disableHostCheck: true
-  },
-}
+    configureWebpack: {
+      plugins: [
+        ...vuetifyOptional,
+      ],
+    },
+    transpileDependencies: vuetifyOptional.length > 0 ? ["vuetify"] : [],
+    devServer: {
+      public: '0.0.0.0:8080',
+      disableHostCheck: true
+    }
+  
+  }
