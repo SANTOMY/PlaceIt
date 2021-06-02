7f4e98ff72c613e17cf67bb8912d9554bada7859
Yamate Kyosuke <ttr.knnb11@ab.auone-net.jp>
2021-06-02 15:24:59 +0900

    Fix conflict

--- front/src/components/Map/Map.js
+++ front/src/components/Map/Map.js
@@ -73,11 +73,11 @@ export default {
                 spots.forEach(spot => {
                     if(this.culSpotRating(spot.spot_id,review) > rating){
                         var marker =  L.marker([spot.y, spot.x], 
-                            {icon: L.icon.glyph({ prefix: 'mdi', glyph: icon_dict[spot.spot_type.split(",")[0]], color: color_dict[spot.spot_type] }) })
+                            {icon: L.icon.glyph({ prefix: 'mdi', glyph: icon_dict[spot.spot_type.split(",")[0]], color: color_dict[spot.spot_type.split(",")[0]] }) })
                         .on('click', this.markerClickEvent);
                         marker.spot_name = spot.spot_name;
                         marker.spot_id = spot.spot_id;
-                        marker.spot_type = spot.spot_type;
+                        marker.spot_type = spot.spot_type.split(",")[0];
                         marker.spot_picture = spot.spot_picture;
                         markerSet.push(marker)
                     }
