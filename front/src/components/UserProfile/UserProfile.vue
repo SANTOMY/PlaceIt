939b25452ddb2f14e9c8e4c2981ba409dec3f508
tsukasa2 <t.nakamura@posl.ait.kyushu-u.ac.jp>
2021-06-16 16:10:16 +0900

    各関数をUserProfile.vueに統合（ほぼたかたくん）

--- front/src/components/UserProfile/UserProfile.vue
+++ front/src/components/UserProfile/UserProfile.vue
@@ -19,8 +19,12 @@
 
 import userProfileDetail from "./UserProfileDetail.vue";
 import {getProfileImage} from "../../routes/imageRequest"
-import {SpotExampleData} from "../share/SpotExampleData";
-import {getSpotByUserId,getSpotYouReviewed} from '../share/GetProfileInformation.js';
+import {getSpot} from '../../routes/spotRequest';
+import {average} from '../../routes/reviewRequest';
+import {getReviewBySpotId, getReviewByUserId} from '../../routes/reviewRequest';
+import {getSpotImage} from "../../routes/imageRequest"
+// import {SpotExampleData} from "../share/SpotExampleData";
+// import {getSpotByUserId,getSpotYouReviewed,getRecommendedSpots} from '../share/GetProfileInformation.js';
 
 export default {
 
@@ -34,7 +38,7 @@ export default {
                 // ユーザー仮データ
             },
             isLoading: true,
-            spot: SpotExampleData(),
+            spot: [],
             my_spot: [
                 // 自分の作成したスポット
                 // required attribute: name, src, good
@@ -75,36 +79,206 @@ export default {
             return
         }
         
-        this.show_count = 0
         getProfileImage( this.user.id )
             .then(result => {
                 // console.log('test, UserProfile getProfileImage success')
                 if(!result.success) return;
                 this.user.src = "data:image/jpeg;base64," + result.data.image;
-            }) 
-
-        getSpotByUserId( this.user.id )
-            .then( result =>{
-                this.show_count += 1
-                this.my_spot = result
-        })
-
-        getSpotYouReviewed( this.user.id )
-            .then( result =>{
-                this.show_count += 1
-                this.good_spot = result
             })
+        
+        this.getSpotByUserId( this.user.id )
+        this.getSpotYouReviewed( this.user.id )
+        this.getRecommendedSpots( this.user.university )
+    },
+    methods: {    
+        getSpotByUserId: async function(user_id){ // 作成スポット取得関数
+            getSpot('', '', '', user_id, '').then(result => {
+                const spot_length = result.spots.length
+                var i = 0;
+                for( var spt of result.spots ){
+                    const spt_id = spt.spot_id;
+                    const name = spt.spot_name;
+
+                    // レビューの計算
+                    getReviewBySpotId(spt_id).then(result => {
+                        const scores = [];
+                        for (var rev of result.review) {
+                            scores.push(rev.score);
+                        }
+
+                        const good = Math.round(10 * average(scores)) / 10;
+                        getSpotImage(spt_id).then((result) => {
+                            if (result.success && result.data != undefined) {
+                                const src = "data:image/jpeg;base64," + result.data[0].image
+                                this.my_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                            } else {
+                                const src = require("@/assets/noimage.png");
+                                this.my_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                            }
+                        }).catch((exception) => {
+                            console.log("Error in getSpotImage: ", exception)
+                            const src = require("@/assets/noimage.png");
+                            this.my_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                        }).finally(()=>{
+                            i += 1;
+                            if (i==spot_length){
+                                this.show_count +=1;
+                                this.sortSpotsByScore( this.my_spot )
+                                console.log('success getSpotByUserId')
+                            }
+                        })
+                    }).catch((exception) => {
+                        console.log("Error in getReviewBySpotId: ", exception)
+                    })
+                }
+            }).catch((exception) => {
+                console.log( "Error in getSpotByUserId: ", exception );
+            })
+        },
+
+        getSpotYouReviewed: async function( user_id ){ //レビューしたスポット取得関数
+            getReviewByUserId( user_id ).then( result => {
+                var reviewd_spot_ids = new Set()
+                for( let rev of result.review ){
+                    reviewd_spot_ids.add( rev.spot_id );
+                }
+                // const all = reviewd_spot_ids.size;
+                // var added = 0;
+                const spot_length = reviewd_spot_ids.size
+                var i = 0;
+                for( let reviewd_spot_id of reviewd_spot_ids ){
+                    getSpot( reviewd_spot_id, '', '', '', '' ).then( result => {
+                        const spt = result.spots[ 0 ];
+                        const spt_id = spt.spot_id;
+                        const name = spt.spot_name;
+        
+                        // レビューの計算
+                        getReviewBySpotId(spt_id).then(result => {
+                            const scores = [];
+                            for (var rev of result.review) {
+                                scores.push(rev.score);
+                            }
+                            const good = Math.round(10 * average(scores)) / 10;
+                            getSpotImage(spt_id).then((result) => {
+                                if (result.success && result.data != undefined) {
+                                    const src = "data:image/jpeg;base64," + result.data[0].image
+                                    this.good_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                                } else {
+                                    const src = require("@/assets/noimage.png");
+                                    this.good_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                                }
+                            }).catch((exception) => {
+                                console.log("Error in getSpotImage: ", exception)
+                                const src = require("@/assets/noimage.png");
+                                this.good_spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                            }).finally(()=>{
+                                i += 1;
+                                if (i==spot_length){
+                                    this.show_count +=1;
+                                    this.sortSpotsByScore( this.good_spot )
+                                    console.log("success getSpotYouReviewed ")
+                                }
+                            })
+                        }).catch((exception => {
+                            console.log("Error in getReviewBySpotId: ", exception);
+                        }));
+                    } ).catch((exception) => {
+                        console.log( "Error in getSpot: ", exception );
+                    })
+                }
+            } ).catch((exception) => {
+                console.log( "Error in getReviewByUserId: ", exception );
+            })
+        },
+
+        getLatestSpots: function( left = 0, right ,spot){ //スポットが多すぎるときの処理。
+            // TODO: 他のスポットにも対応
+            console.log( "latestSpots", ( spot ).slice( left, right ) )
+            return ( spot ).slice( left, right )
+        },
+
+        shuffleArray: async function( array ){ // 配列をシャッフルする関数
+            var newArray = [];
+            while ( array.length > 0 ) {
+                var n = array.length;
+                var k = Math.floor( Math.random() * n );
+
+                newArray.push( array[ k ] );
+                array.splice( k, 1 );
+            }
+            return newArray;
+        },
+
+        sortSpotsByScore: async function( array ){ // スポットをスコアの高い順にソートする関数
+            array.sort( function( a, b ){
+                if( a.good > b.good ){
+                    return -1
+                }else{
+                    return 1
+                }
+            } )
+        },
+
+        getRecommendedSpots: async function(user_univ){ // おすすめスポット取得関数
+            getSpot('', '', '', '', user_univ).then( result => {
+                this.shuffleArray( result.spots ).then(result =>{
+                    const spot_length = result.length
+                    var i = 0;
+                    for( var spt of result ){
+                        const spt_id = spt.spot_id;
+                        const name = spt.spot_name;
+
+                        // レビューの計算
+                        getReviewBySpotId(spt_id).then(result => {
+                            const scores = [];
+                            for (var rev of result.review) {
+                                scores.push(rev.score);
+                            }
+                            const good = Math.round(10 * average(scores)) / 10;
+                            
+                            getSpotImage(spt_id).then((result) => {
+                                if (result.success && result.data != undefined) {
+                                    const src = "data:image/jpeg;base64," + result.data[0].image
+                                    this.spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                                } else {
+                                    const src = require("@/assets/noimage.png");
+                                    this.spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                                }
+                            }).catch((exception) => {
+                                console.log("Error in getSpotImage: ", exception)
+                                const src = require("@/assets/noimage.png");
+                                this.spot.push({ "spotId": spt_id, "name": name, "src": src, "good": good });
+                            }).finally(()=>{
+                                i += 1;
+                                this.sortSpotsByScore( this.spot )
+                                if (i==spot_length){
+                                    console.log("success getRecommendedSpots ")
+                                    this.show_count +=1;
+                                }
+                            })
+                        }).catch((exception => {
+                            console.log("Error in getReviewBySpotId: ", exception)
+                        }));
+                    }        
+                })
+            }).catch((exception) => {
+                console.log( "Error in getSpot: ", exception );
+            })
+        },
     },
     watch:  {
         show_count: function() {
-            if(this.show_count!=2) return;
-            if(this.show_count==2) {
+            if(this.show_count!=3) {
+                return
+            }
+
+            if(this.show_count==3) {
                 this.isLoading=false
-                // console.log('=========finish loading===========')
-                // console.log('my_spot',this.my_spot)
-                // console.log('good_spot',this.good_spot)
-                // console.log('user',this.user)
-                // console.log('otherUser',this.otherUser)
+                console.log('=========finish loading===========')
+                console.log('my_spot',this.my_spot)
+                console.log('good_spot',this.good_spot)
+                console.log('spot',this.spot)
+                console.log('==================================')
             }
         },
     },
