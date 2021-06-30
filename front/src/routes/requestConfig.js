cca7f7581ee5f51d71dc07f2cb216e1f2632f8bc
ShimizuKazuki <shimizu@posl.ait.kyushu-u.ac.jp>
2021-06-30 15:39:02 +0900

    revert pull

--- front/src/routes/requestConfig.js
+++ front/src/routes/requestConfig.js
@@ -1,3 +1,2 @@
 
-//export const serverIP = process.platform === 'darwin' ? 'http://0.0.0.0:5000': 'http://127.0.0.1:5000';
-export const serverIP = 'https://place-it-back.herokuapp.com';
+export const serverIP = process.platform === 'darwin' ? 'http://0.0.0.0:5000': 'http://127.0.0.1:5000';
